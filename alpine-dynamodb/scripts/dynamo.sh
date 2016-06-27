9dd5d84ae5777199f77b29beef863c1ff664cc36
Erik Straub <erik.straub@oddnetworks.com>
2016-06-27 12:13:00 -0400

    Updates alpine-dynamo with -sharedDb and a -dbPath volume

--- alpine-dynamodb/scripts/dynamo.sh
+++ alpine-dynamodb/scripts/dynamo.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-exec java -Djava.library.path=/dynamo/DynamoDBLocal_lib -jar DynamoDBLocal.jar $@
+exec java -Djava.library.path=/dynamo/DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb $@
